2bbb6ef9feefef9519e554d0073ec6eb42985c36
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-01-17 00:05:13 +0900

    Share! Edit Function Prototype
    TODO: Adding Security Feature, Working Test

--- process/edit.php
+++ process/edit.php
@@ -15,11 +15,17 @@
   if (!empty($_POST['text'])) {
       $text = $conn->real_escape_string($_POST['text']);
       $id = $_GET['id'];
-      $udb = 'notedb_'.$_SESSION['pid'];
+      $pid = $_SESSION['pid'];
+      if (count(explode('_', $id)) > 1) {
+          $pid = explode('_', $id)[0];
+          $id = explode('_', $id)[1];
+          $sql = "SELECT shareEdit FROM sharedb_$pid WHERE id LIKE ".$_GET['id'];
+      }
+      $udb = "notedb_$pid";
       $sql = "UPDATE $udb SET name='$name', text='$text', edittime=now() WHERE id='$id'";
       $result = $conn->query($sql);
       $_SESSION['confirm_edit'] = 'confirm';
-      header('Location: ../complete/edit.php?id='.$id);
+      header("Location: ../complete/edit.php?id=$id");
   } else {
       echo "<script>window.alert('본문이 입력되지 않았습니다.');</script>";
       echo "<script>window.location=('../note.php?id=".$_GET['id']."');</script>";
